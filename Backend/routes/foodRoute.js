c64069c6269f19a1a5a7620de913457b81a8520a
git-omar-hub <the.omar.elbasty@gmail.com>
2026-03-01 11:47:53 +0200

    Add files via upload

--- Backend/routes/foodRoute.js
+++ Backend/routes/foodRoute.js
@@ -7,19 +7,33 @@ import {
 import multer from "multer";
 import { count } from "console";
 import path from "path";
+import cloudinary from "../config/cloudinary.js";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 
 const foodRouter = express.Router();
 
 // image storge engine
-const storge = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads");
-  },
-  filename: (req, file, cb) => {
-    return cb(null, `${Date.now()}${file.originalname}`);
+// const storge = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "uploads");
+//   },
+//   filename: (req, file, cb) => {
+//     return cb(null, `${Date.now()}${file.originalname}`);
+//   },
+// });
+// const upload = multer({ storage: storge });
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "foods", // هيعمل فولدر في Cloudinary
   },
 });
-const upload = multer({ storage: storge });
+
+const upload = multer({ storage: storage });
+
+
+
 foodRouter.post("/add", upload.single("image"), addFood);
 foodRouter.get("/list", listFood);
 foodRouter.post("/remove", removeFood);
